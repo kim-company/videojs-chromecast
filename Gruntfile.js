60c6008c3941b1c116020d90ff50891d037dc183
Philip Giuliani <giiuliio@gmail.com>
2014-09-15 20:14:01 +0200

    Initial commit
    First commit of VideoJS Chromecast Plugin. Its a fork of
    https://github.com/benjipott/video.js-chromeCast but it will be
    completely rewritten and updated to the latest version of videojs.

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+
+    clean: {
+      files: ['dist']
+    },
+
+    concat: {
+      options: {
+        separator: ''
+      },
+      dist: {
+        src: ['src/videojs.pluginBase.js','src/videojs.chromecast.js', 'src/videojs.chromecastComponent.js', 'src/videojs.chromecastTech.js'],
+        dest: 'dist/videojs.chromecast.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        src: 'dist/videojs.chromecast.js',
+        dest: 'dist/videojs.chromecast.min.js'
+      },
+    },
+
+    copy: {
+      dist: {
+        src: 'src/videojs.chromecast.css',
+        dest: 'dist/videojs.chromecast.css'
+      }
+    },
+
+    cssmin: {
+      dist: {
+        src: 'src/videojs.chromecast.css',
+        dest: 'dist/videojs.chromecast.min.css'
+      }
+    },
+
+    usebanner: {
+      taskName: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+              'dist/videojs.chromecast.js',
+              'dist/videojs.chromecast.min.js',
+              'dist/videojs.chromecast.min.css',
+              'dist/videojs.chromecast.css'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-banner');
+
+  grunt.registerTask('default', ['clean', 'concat', 'uglify', 'copy', 'cssmin', 'usebanner']);
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# VideoJS Chromecast Plugin
+
+README TODO.
+
+[![david-dm](http://img.shields.io/david/kim-company/videojs-chromecast.svg?style=flat)](https://david-dm.org/kim-company/videojs-chromecast/)
